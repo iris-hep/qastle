56cffe13e92451cea79b03a27eb3822c9d9f917b
Mason Proffitt <mproffit@cern.ch>
2019-10-08 15:12:39 +0200

    fix ast parsing

--- ast_language.py
+++ ast_language.py
@@ -1,43 +1,47 @@
 import lark
 
 import ast
+import os
 
-grammar_pathname = 'grammar.lark'
+grammar_pathname = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'grammar.lark')
 
 with open(grammar_pathname) as grammar_file:
   parser = lark.Lark(grammar_file.read(), start='record')
 
-class TextToPython(lark.Transformer):
-  def record(self, record_tree):
-    if len(record_tree) == 0 or isinstance(record[0], lark.Token) and record[0].type == 'WHITESPACE':
+class TextASTToPythonAST(lark.Transformer):
+  def record(self, children):
+    if len(children) == 0 or isinstance(children[0], lark.Token) and children[0].type == 'WHITESPACE':
       return ast.Module(body=[])
+    return ast.Module(body=[ast.Expr(value=children[0])])
+
+  def expression(self, children):
+    for child in children:
+      if not (isinstance(child, lark.Token) and child.type == 'WHITESPACE'):
+        return child
+    raise SyntaxError('Expression does not contain a node')
+
+  def atom(self, children):
+    child = children[0]
+    if child.type == 'IDENTIFIER':
+      return ast.Name(id=child.value, ctx=ast.Load())
+    elif child.type == 'STRING_LITERAL':
+      return ast.Str(s=ast.literal_eval(child.value))
+    elif child.type == 'NUMERIC_LITERAL':
+      return ast.Num(n=ast.literal_eval(child.value))
     else:
-      return ast.Module(body=[ast.Expr(value=record[0])])
+      raise Exception("Unknown atom child type")
 
-  def expression(self, expression_tree):
-    for element in expression_tree:
-      if isinstance(element, lark.Token) and element.type == 'WHITESPACE':
-        pass
-      else:
-        return element
+  #def composite(self, children):
 
-  def atom(self, atom_tree):
-    token = atom_tree[0]
-    if token.type == 'NAME':
-      return ast.Name(id=token.value, ctx=ast.Load())
-    elif token.type == 'NUMERIC_LITERAL':
-      return ast.Num(n=ast.literal_eval(token.value))
-    elif token.type == 'STRING_LITERAL':
-      return ast.Str(s=ast.literal_eval(token.value))
-    else:
-      raise Exception("Unknown atom token type")
-
-  def composite(self, composite_tree):
-    return composite_tree
-
-class PythonToText(ast.NodeVisitor):
+class PythonASTToTextAST(ast.NodeVisitor):
   def visit_Module(self, node):
-    return '\n'.join([self.visit(child_node) for child_node in node.body])
+    n_children = len(node.body)
+    if n_children == 0:
+      return '';
+    elif n_children == 1:
+      return self.visit(node.body[0])
+    else:
+      raise SyntaxError('A record must contain zero or one expressions; found ' + str(n_children))
 
   def visit_Expr(self, node):
     return self.visit(node.value)
@@ -50,3 +54,10 @@ class PythonToText(ast.NodeVisitor):
 
   def visit_Str(self, node):
     return repr(node.s)
+
+def text_ast_to_python_ast(text_ast):
+  tree = parser.parse(text_ast)
+  return TextASTToPythonAST().transform(tree)
+
+def python_ast_to_text_ast(python_ast):
+  return PythonASTToTextAST().visit(python_ast)
