55f7ea4cbefc239458c976d9f1d46f42ef5bf843
Mason Proffitt <mproffit@cern.ch>
2019-10-10 20:50:00 +0200

    add more setup.py metadata

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ import setuptools
 
 setuptools.setup(name='ast_language',
                  version=0.1,
-                 packages=setuptools.find_packages())
+                 packages=setuptools.find_packages(),
+                 author='Mason Proffitt',
+                 author_email='masonlp@uw.edu',
+                 url='https://github.com/iris-hep/ast-language')
