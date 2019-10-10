7e400a9a4221e94e8cd46d0263c9e06a2989e52b
Mason Proffitt <mproffit@cern.ch>
2019-10-10 21:53:51 +0200

    add lark requirement in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import setuptools
 setuptools.setup(name='ast_language',
                  version=0.1,
                  packages=setuptools.find_packages(),
+                 install_requires=['lark-parser'],
                  author='Mason Proffitt',
                  author_email='masonlp@uw.edu',
                  url='https://github.com/iris-hep/ast-language')
