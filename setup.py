53f98ca845ee2c0afa3effcfc79aa6ccba7173dd
Mason Proffitt <mproffit@cern.ch>
2019-10-10 22:00:00 +0200

    pull in grammar file in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setuptools.setup(name='ast_language',
                  version=0.1,
                  packages=setuptools.find_packages(),
                  install_requires=['lark-parser'],
+                 package_data={'ast_language': ['grammar.lark']},
                  author='Mason Proffitt',
                  author_email='masonlp@uw.edu',
                  url='https://github.com/iris-hep/ast-language')
