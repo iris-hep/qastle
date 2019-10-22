85a204757c230fa3b09ae4463b5bea5a082353a5
Mason Proffitt <mproffit@cern.ch>
2019-10-22 07:44:41 +0200

    add SelectMany support

--- ast_language/linq_util.py
+++ ast_language/linq_util.py
@@ -10,6 +10,13 @@ class Select(ast.AST):
         self.selector = selector
 
 
+class SelectMany(ast.AST):
+    def __init__(self, source, selector):
+        self._fields = ['source', 'selector']
+        self.source = source
+        self.selector = selector
+
+
 class Where(ast.AST):
     def __init__(self, source, predicate):
         self._fields = ['source', 'predicate']
@@ -28,6 +35,14 @@ class InsertLINQNodesTransformer(ast.NodeTransformer):
                 if not isinstance(node.args[0], ast.Lambda):
                     raise SyntaxError('Select() call argument must be a lambda')
                 return Select(source=node.func.value, selector=node.args[0])
+            elif node.func.attr == 'SelectMany':
+                if len(node.args) != 1:
+                    raise SyntaxError('SelectMany() call must have exactly one argument')
+                if isinstance(node.args[0], ast.Str):
+                    node.args[0] = unwrap_ast(ast.parse(node.args[0].s))
+                if not isinstance(node.args[0], ast.Lambda):
+                    raise SyntaxError('SelectMany() call argument must be a lambda')
+                return SelectMany(source=node.func.value, selector=node.args[0])
             elif node.func.attr == 'Where':
                 if len(node.args) != 1:
                     raise SyntaxError('Where() call must have exactly one argument')
