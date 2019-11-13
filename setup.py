4c42b71c15d02e5273e089cb0714a59e7a6d8705
Mason Proffitt <mproffit@cern.ch>
2019-11-13 15:36:22 +0100

    fix setup.py after grammar.lark rename

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(name='ast_language',
                  version=0.1,
                  packages=setuptools.find_packages(exclude=['tests']),
                  install_requires=['lark-parser>=0.6.5'],
-                 package_data={'ast_language': ['grammar.lark']},
+                 package_data={'ast_language': ['syntax.lark']},
                  author='Mason Proffitt',
                  author_email='masonlp@uw.edu',
                  url='https://github.com/iris-hep/ast-language')
