c57fb6eecd5cc84200e66f166304e05b136911c3
Mason Proffitt <mproffit@cern.ch>
2020-01-13 10:42:25 +0100

    keep checks on LINQ operator fields

--- qastle/linq_util.py
+++ qastle/linq_util.py
@@ -87,13 +87,69 @@ class InsertLINQNodesTransformer(ast.NodeTransformer):
                 return self.generic_visit(node)
             source = node.args[0]
             args = node.args[1:]
-        if function_name in ('Where', 'Select', 'SelectMany') and isinstance(args[0], ast.Str):
-            args[0] = unwrap_ast(ast.parse(node.args[0].s))
-        elif function_name == 'Aggregate' and isinstance(args[1], ast.Str):
-            args[1] = unwrap_ast(ast.parse(args[1].s))
-        source = self.visit(source)
-        args = [self.visit(arg) for arg in args]
-        return globals()[function_name](source, *args)
+        else:
+            return self.generic_visit(node)
+
+        if function_name == 'Where':
+            if len(args) != 1:
+                raise SyntaxError('Where() call must have exactly one argument')
+            if isinstance(args[0], ast.Str):
+                args[0] = unwrap_ast(ast.parse(args[0].s))
+            if not isinstance(args[0], ast.Lambda):
+                raise SyntaxError('Where() call argument must be a lambda')
+            return Where(source=self.visit(source),
+                         predicate=self.visit(args[0]))
+        elif function_name == 'Select':
+            if len(args) != 1:
+                raise SyntaxError('Select() call must have exactly one argument')
+            if isinstance(args[0], ast.Str):
+                args[0] = unwrap_ast(ast.parse(args[0].s))
+            if not isinstance(args[0], ast.Lambda):
+                raise SyntaxError('Select() call argument must be a lambda')
+            return Select(source=self.visit(source),
+                          selector=self.visit(args[0]))
+        elif function_name == 'SelectMany':
+            if len(args) != 1:
+                raise SyntaxError('SelectMany() call must have exactly one argument')
+            if isinstance(args[0], ast.Str):
+                args[0] = unwrap_ast(ast.parse(args[0].s))
+            if not isinstance(args[0], ast.Lambda):
+                raise SyntaxError('SelectMany() call argument must be a lambda')
+            return SelectMany(source=self.visit(source),
+                              selector=self.visit(args[0]))
+        elif function_name == 'First':
+            if len(args) != 0:
+                raise SyntaxError('First() call must have zero arguments')
+            return First(source=self.visit(source))
+        elif function_name == 'Aggregate':
+            if len(args) != 2:
+                raise SyntaxError('Aggregate() call must have exactly two arguments; found'
+                                  + str(len(args)))
+            if isinstance(args[1], ast.Str):
+                args[0] = unwrap_ast(ast.parse(args[0].s))
+            if not isinstance(args[1], ast.Lambda):
+                raise SyntaxError('Second Aggregate() call argument must be a lambda')
+            return Aggregate(source=self.visit(source),
+                             seed=self.visit(args[0]),
+                             func=self.visit(args[1]))
+        elif function_name == 'Count':
+            if len(args) != 0:
+                raise SyntaxError('Count() call must have zero arguments')
+            return Count(source=self.visit(source))
+        elif function_name == 'Max':
+            if len(args) != 0:
+                raise SyntaxError('Max() call must have zero arguments')
+            return Max(source=self.visit(source))
+        elif function_name == 'Min':
+            if len(args) != 0:
+                raise SyntaxError('Min() call must have zero arguments')
+            return Min(source=self.visit(source))
+        elif function_name == 'Sum':
+            if len(args) != 0:
+                raise SyntaxError('Sum() call must have zero arguments')
+            return Sum(source=self.visit(source))
+        else:
+            raise NameError('Unhandled LINQ operator: ' + function_name)
 
 
 def insert_linq_nodes(python_ast):
