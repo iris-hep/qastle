4617415f0e79846cac4860411353fcb2ff0fca83
Mason Proffitt <mproffit@cern.ch>
2020-07-22 15:22:30 +0200

    move to version 0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='qastle',
-                 version=0.7,
+                 version=0.8,
                  description='Query AST Language Expressions',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
