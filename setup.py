0ed3f34173300adc97e86f6eea122719e33884a5
Mason Proffitt <mason.proffitt@cern.ch>
2020-11-11 11:59:02 +0100

    add python_requires

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setuptools.setup(name='qastle',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
                  packages=setuptools.find_packages(exclude=['tests']),
+                 python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*', '<3.10'
                  install_requires=['lark-parser>=0.6.5'],
                  package_data={'qastle': ['syntax.lark']},
                  author='Mason Proffitt',
