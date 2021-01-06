d40e81cce6e374d14a31e7a11d62ce36e3c182a9
Mason Proffitt <mason.proffitt@cern.ch>
2021-01-06 16:50:50 +0100

    eliminate requirements.txt, simplify CI

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(name='qastle',
                  python_requires=('>=2.7, '
                                   '!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <3.10'),
                  install_requires=['lark-parser>=0.6.5'],
+                 extras_require={'test': ['flake8', 'pytest']}
                  package_data={'qastle': ['syntax.lark']},
                  author='Mason Proffitt',
                  author_email='masonlp@uw.edu',
