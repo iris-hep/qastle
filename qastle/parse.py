a0c0e085e6dda6e198259160d0d44b32de481d94
Mason Proffitt <mason.proffitt@cern.ch>
2023-10-03 13:16:13 +0200

    update lark-parser to lark

--- qastle/parse.py
+++ qastle/parse.py
@@ -7,7 +7,7 @@ syntax_specification_pathname = os.path.join(os.path.dirname(os.path.abspath(__f
                                              'syntax.lark')
 
 with open(syntax_specification_pathname) as syntax_specification_file:
-    Parser = lark.Lark(syntax_specification_file.read(), start='record')
+    Parser = lark.Lark(syntax_specification_file.read(), start='record', maybe_placeholders=False)
 
 
 def parse(text):
