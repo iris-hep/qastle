bb54afd65f5b7c2dca168df069a0a8de55408b23
Matthew Feickert <matthew.feickert@cern.ch>
2024-03-19 00:40:38 -0500

    build: Add max supported CPython version guard
    * Numba does a similar thing in its seutup.py. This was recommended by
      Henry Schreiner as an alternative to placing a version cap on the
      requires-python metadata.

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
+import sys
 import setuptools
+
+
+# Taken from Numba
+def _guard_python_version(max_python):
+    version_module = None
+    try:
+        from packaging import version as version_module
+    except ImportError:
+        try:
+            from setuptools._vendor.packaging import version as version_module
+        except ImportError:
+            pass
+
+    if version_module is None:
+        return
+
+    current_python = version_module.parse(".".join(map(str, sys.version_info[:3])))
+    max_python = version_module.parse(max_python)
+
+    if not current_python < max_python:
+        raise RuntimeError(
+            f"Cannot install on Python version {current_python} as Python {max_python}+ is not yet supported."
+        )
+
+
+_guard_python_version(max_python="3.13")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
